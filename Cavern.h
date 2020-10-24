cd2fbc17c8427bc8edc68340538117ad747f5417
Taylor Courtney <taylorcourtney2000@googlemail.com>
2020-10-24 15:21:37 +0100

    Added Cavern class and read .cav method template

---
+++ Cavern.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Cavern {
+public:
+	int size;
+	int** coords;
+	int** connections;
+};
--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
+#include<fstream>
+#include "Cavern.h"
 
 using namespace std;
 
+Cavern* readCAV(char* path) {
+	ifstream file(path);
+	Cavern* cavern = new Cavern();
+	if (file.is_open()) {
+
+	}
+	return cavern;
+}
+
 int main(int argc, char **argv) {
-  cout << "Hello, world!";
-  return 0;
+	if(argc == 1)
+		readCAV(argv[0]);
+	else
+		cout << "(!) invalid number of parameters";
+	return 0;
 }
