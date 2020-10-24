fd5fa4af3924f249491eb6d113f5d202496b68e1
Taylor Courtney <taylorcourtney2000@googlemail.com>
2020-10-24 15:51:14 +0100

    Fixed command line parameter requirement

--- main.cpp
+++ main.cpp
@@ -1,20 +1,25 @@
 #include<iostream>
 #include<fstream>
+#include<string>
 #include "Cavern.h"
 
 using namespace std;
 
 Cavern* readCAV(char* path) {
-	ifstream file(path);
+	ifstream file (path);
 	Cavern* cavern = new Cavern();
 	if (file.is_open()) {
-
+		//string line;
+		//while (!file.eof()) {
+		//	//file >> line;
+		//	line.push_back(file.get());
+		//}
 	}
 	return cavern;
 }
 
 int main(int argc, char **argv) {
-	if(argc == 1)
+	if(argc == 2)
 		readCAV(argv[0]);
 	else
 		cout << "(!) invalid number of parameters";
