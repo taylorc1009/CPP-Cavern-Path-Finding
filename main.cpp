3d229bbf78d4394a5989b4364a0c2a0f395c360e
Taylor Courtney <taylorcourtney2000@googlemail.com>
2020-10-26 10:39:03 +0000

    Potential .cav reader almost implemented

--- main.cpp
+++ main.cpp
@@ -1,19 +1,43 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include <vector>
+#include <sstream>
 #include "Cavern.h"
 
 using namespace std;
 
 Cavern* readCAV(char* path) {
-	ifstream file (path);
+	//ifstream file (path);
+	fstream file(path);
 	Cavern* cavern = new Cavern();
-	if (file.is_open()) {
-		//string line;
-		//while (!file.eof()) {
-		//	//file >> line;
-		//	line.push_back(file.get());
-		//}
+	/*if (file.is_open()) {
+		string line;
+		while (file >> line) {
+			while(line.;
+		}
+	}*/
+	std::string token, line;
+	std::stringstream convertor(line);
+	int tokens = 0;
+	bool isY = false;
+	while (std::getline(convertor, token, ',')) {
+		tokens++;
+		if (tokens == 1) { //declares the Cavern object
+			cavern->size = std::stoi(token);
+			cavern->coords = new int*[cavern->size];
+			for (int i = 0; i < cavern->size; i++)
+				cavern->coords[i] = new int[2];
+			cavern->connections = new int*[cavern->size];
+			for (int i = 0; i < cavern->size; i++)
+				cavern->coords[i] = new int[cavern->size];
+		}
+		else if (tokens < (cavern->size * 2) + 1) {
+			cavern->coords[isY ? 1 : 0][isY ? tokens - 2 : tokens - 1] = std::stoi(token);
+			isY = !isY;
+		}
+		else
+			cavern->connections[tokens]
 	}
 	return cavern;
 }
