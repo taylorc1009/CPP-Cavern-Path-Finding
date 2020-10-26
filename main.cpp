fca34e1e3deb6635f55ff8d43c98a0e4e2d081d6
Taylor Courtney <taylorcourtney2000@googlemail.com>
2020-10-26 16:49:49 +0000

    Fixed declaration of the cavern->coords matrix when reading from CAV

--- main.cpp
+++ main.cpp
@@ -8,43 +8,57 @@
 using namespace std;
 
 Cavern* readCAV(char* path) {
-	//ifstream file (path);
 	fstream file(path);
 	Cavern* cavern = new Cavern();
-	/*if (file.is_open()) {
-		string line;
-		while (file >> line) {
-			while(line.;
+
+	if (file.is_open()) {
+		std::string tok;
+		int i = 0, row = 0, col = 0;
+		bool isY = false;
+
+		while (std::getline(file, tok, ',')) {
+			if (i > (cavern->size * 2) && i <= (cavern->size * cavern->size)) {
+				cavern->connections[row][col] = std::stoi(tok);
+			}
+			else if (i > 0 && i <= (cavern->size * 2)) {
+				cavern->coords[isY ? 1 : 0][col] = std::stoi(tok);
+				if (isY)
+					col++;
+				if (i == (cavern->size * 2) + 1)
+					col = 0; //resets col for the connections declaration count
+				isY = !isY;
+			}
+			else if (i == 0) { //declares the Cavern object
+				cavern->size = std::stoi(tok);
+				cavern->coords = new int* [cavern->size];
+				for (int i = 0; i < cavern->size; i++)
+					cavern->coords[i] = new int[2];
+				cavern->connections = new int* [cavern->size];
+				for (int i = 0; i < cavern->size; i++)
+					cavern->coords[i] = new int[cavern->size];
+			}
+			i++;
 		}
-	}*/
-	std::string token, line;
-	std::stringstream convertor(line);
-	int tokens = 0;
-	bool isY = false;
-	while (std::getline(convertor, token, ',')) {
-		tokens++;
-		if (tokens == 1) { //declares the Cavern object
-			cavern->size = std::stoi(token);
-			cavern->coords = new int*[cavern->size];
-			for (int i = 0; i < cavern->size; i++)
-				cavern->coords[i] = new int[2];
-			cavern->connections = new int*[cavern->size];
-			for (int i = 0; i < cavern->size; i++)
-				cavern->coords[i] = new int[cavern->size];
-		}
-		else if (tokens < (cavern->size * 2) + 1) {
-			cavern->coords[isY ? 1 : 0][isY ? tokens - 2 : tokens - 1] = std::stoi(token);
-			isY = !isY;
-		}
-		else
-			cavern->connections[tokens]
 	}
+	else
+		cout << "(!) file failed to open: " << path;
+	//cout << cavern->size;
+	//std::cin.ignore();
+	//string y;
+	//while (true) {
+	//	cout << "\nWhich y?";
+	//	std::getline(std::cin, y);
+	//	cout << "(" << cavern->coords[0][std::stoi(y)] << "," << cavern->coords[1][std::stoi(y)] << ")";
+	//}
+	///*for (int i = 0; i < cavern->size; i++)
+	//	cout << "(" << cavern->coords[0][i] << "," << cavern->coords[1][i] << "), ";
+	//std::cin.ignore();*/
 	return cavern;
 }
 
 int main(int argc, char **argv) {
 	if(argc == 2)
-		readCAV(argv[0]);
+		readCAV(argv[1]);
 	else
 		cout << "(!) invalid number of parameters";
 	return 0;
