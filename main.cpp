50045c95c3ab84c44cd3a6abbc42126a58c4122b
Taylor Courtney <taylorcourtney2000@googlemail.com>
2020-10-29 09:46:27 +0000

    Fixed crashing during storing point's coordinates (had an index out of bounds error)

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ void freeCavern(Cavern* cavern) {
 }
 
 void readCAV(char* path, Cavern* cavern) {
-	fstream file(path);
+	ifstream file(path);
 
 	if (file.is_open()) {
 		std::string tok;
@@ -27,31 +27,24 @@ void readCAV(char* path, Cavern* cavern) {
 
 		while (std::getline(file, tok, ',')) {
 			if (i > (cavern->size * 2) && i <= ((cavern->size * cavern->size) + (cavern->size * 2))) {
-				/*if(col == 0)
-					cout << "\n";
-				else
-					cout << col;*/
-				cavern->connections[row][col] = std::stoi(tok);
-				//cout << cavern->connections[row][col];
-				col++;
-				if (col == cavern->size) {
-					row++;
-					col = 0;
-					//cout << "\n";
+				cavern->connections[row][col] = stoi(tok);
+				row++;
+				if (row == cavern->size) {
+					col++;
+					row = 0;
 				}
 			}
-			else if (i > 0 && i <= (cavern->size * 2) + 1) {
-				cavern->coords[isY ? 1 : 0][col] = std::stoi(tok);
+			else if (i > 0 && i <= (cavern->size * 2)) {
+				cavern->coords[row][isY ? 1 : 0] = stoi(tok);
 				if (isY)
-					col++;
+					row++;
 				if (i == (cavern->size * 2))
-					col = 0; //resets col for the connections declaration count
+					row = 0; //resets col for the connections declaration count
 				else
 					isY = !isY;
 			}
 			else if (i == 0) { //declares the Cavern object
-				cavern->size = std::stoi(tok);
-				cout << cavern->size << "\n";
+				cavern->size = stoi(tok);
 				cavern->coords = new int* [cavern->size];
 				for (int i = 0; i < cavern->size; i++)
 					cavern->coords[i] = new int[2];
@@ -63,37 +56,13 @@ void readCAV(char* path, Cavern* cavern) {
 		}
 	}
 	else
-		cout << "(!) file failed to open: " << path;
-
-	//cout << cavern->size;
-	//std::cin.ignore();
-	//string y;
-	//while (true) {
-	//	cout << "\nWhich y?";
-	//	std::getline(std::cin, y);
-	//	cout << "(" << cavern->coords[0][std::stoi(y)] << "," << cavern->coords[1][std::stoi(y)] << ")";
-	//}
-	///*for (int i = 0; i < cavern->size; i++)
-	//	cout << "(" << cavern->coords[0][i] << "," << cavern->coords[1][i] << "), ";
-	//std::cin.ignore();*/
-
-	/*cout << "\n";
-	for (int i = 0; i < cavern->size; i++) {
-		for (int j = 0; j < cavern->size; j++)
-			cout << cavern->connections[i][j];
-		cout << "\n";
-	}
-	cout << "done";*/
+		cout << "(!) failed to open file: " << path;
 }
 
 int main(int argc, char **argv) {
 	if (argc == 2) {
 		Cavern* cavern = new Cavern();
 		readCAV(argv[1], cavern);
-		/*for (int i = 0; i < cavern->size; i++) {
-			for (int j = 0; j < cavern->size; j++)
-				cout << cavern->connections[i][j];
-		}*/
 		freeCavern(cavern);
 	}
 	else
