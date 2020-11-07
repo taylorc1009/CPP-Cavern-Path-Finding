65749c7b1313ac40552102c256400489fb0dd1c7
Taylor Courtney <taylorcourtney2000@googlemail.com>
2020-11-07 14:41:36 +0000

    Removed obsolete attributes/methods (were commented out already)

--- Cavern.h
+++ Cavern.h
@@ -3,20 +3,13 @@
 
 class Cavern {
 private:
-	//int id;
 	int x;
 	int y;
 	std::vector<int> connections;
 public:
-	//Cavern(int _id, int _x, int _y) : id(_id), x(_x), y(_y) {}
 	Cavern(int _x, int _y) : x(_x), y(_y) {}
-	//int getID() const { return id; }
 	int getX() const { return x; }
 	int getY() const { return y; }
 	std::vector<int> const &getConnections() const { return connections; }
 	void addConnection(int c) { connections.push_back(c); }
-	/*void deleteConnections() {
-		connections.clear();
-		connections.swap(std::vector<int>(connections));
-	}*/
 };
